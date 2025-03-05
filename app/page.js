8b4ee08989fc444cbbf21ea1d507b5fda80e5fe8
Kartik Bhardwaj <165783489+Kkbhardwaj26@users.noreply.github.com>
2025-03-05 21:01:26 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,3 @@
-export default function Page() {
-  return (
-    <div>
-      <h1>Welcome to My Next.js App</h1>
-      <p>This is a test page for AWS Amplify.</p>
-    </div>
-  );
+export default function Home() {
+  return <h1>Welcome to My Next.js App</h1>;
 }
